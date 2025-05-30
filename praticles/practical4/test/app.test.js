77b4f921c09c2beebedb95c60360f7913598b60a
Namgyel Wangchuk <02230291.cst@rub.edu.bt>
2025-05-30 22:00:33 +0600

    p4 report

--- praticles/practical4/test/app.test.js
+++ praticles/practical4/test/app.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../app');
 
 describe('GET /', () => {
   it('responds with Hello message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello from Node.js!');
+    expect(response.text).toContain('Hello');
   });
 });
